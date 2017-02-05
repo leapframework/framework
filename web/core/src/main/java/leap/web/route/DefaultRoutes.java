53ed42776b3516b7e8bff20e8bdb4977e0871ecc
Evan feng <live.evan@gmail.com>
2017-02-05 22:38:57 +0800

    web core & web api : add two utils methods

--- web/core/src/main/java/leap/web/route/DefaultRoutes.java
+++ web/core/src/main/java/leap/web/route/DefaultRoutes.java
@@ -18,6 +18,7 @@ package leap.web.route;
 import leap.core.annotation.Inject;
 import leap.core.web.path.PathTemplateFactory;
 import leap.lang.Args;
+import leap.lang.New;
 import leap.lang.collection.ArrayIterator;
 import leap.lang.http.HTTP.Method;
 import leap.web.Handler;
@@ -132,7 +133,12 @@ public class DefaultRoutes implements Routes {
 		return this;
     }
 
-	@Override
+    @Override
+    public Route match(String method, String path) {
+        return match(method, path, New.hashMap(), New.hashMap());
+    }
+
+    @Override
     public Route match(String method, String path, Map<String,Object> inParameters,  Map<String, String> outVariables) {
 		Route[] routes = this.array;
 		
--- web/core/src/main/java/leap/web/route/Routes.java
+++ web/core/src/main/java/leap/web/route/Routes.java
@@ -163,9 +163,14 @@ public interface Routes extends Iterable<Route>,Emptiable {
 	 * Adds all the routes. 
 	 */
 	Routes addAll(Iterable<Route> routes);
+
+    /**
+     * Returns the matched {@link Route} or <code>null</code> if no route matched.
+     */
+    Route match(String method, String path);
 	
 	/**
-	 * Returns a matched {@link DefaultRoute} or <code>null</code> if no route matched.
+	 * Returns a matched {@link Route} or <code>null</code> if no route matched.
 	 */
 	Route match(String method,String path,Map<String,Object> inParameters, Map<String,String> outVariables);
 
