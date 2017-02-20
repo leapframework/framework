c8c6b618054533c76dde0b8b10040c4eed0a363b
Kael <penghj@bingosoft.net>
2017-02-20 12:48:49 +0800

    fix date argument bind

--- web/api/src/test/java/tests/model/ArgumentBindControllerTest.java
+++ web/api/src/test/java/tests/model/ArgumentBindControllerTest.java
@@ -32,14 +32,14 @@ public class ArgumentBindControllerTest extends WebTestBase {
                 .addFormParam("TIME_PATTERN","01:59:50")
                 .addFormParam("DATETIME_PATTERN","2017-02-20 01:59:50")
                 .addFormParam("TIMESTAMP_PATTERN","2017-02-20 01:59:50.443")
-                .addFormParam("ISO8601_DATE_PATTERN","2017-02-20T01:59:50.96Z")
-                .addFormParam("RFC3339_DATE_PATTERN1","2017-02-20T01:59:50.960Z")
+                .addFormParam("ISO8601_DATE_PATTERN","2017-02-20T01:59:50.96+0800")
+                .addFormParam("RFC3339_DATE_PATTERN1","2017-02-20T01:59:50.960+0800")
                 .addFormParam("T_DATE_PATTERN","2017-02-20")
                 .addFormParam("T_TIME_PATTERN","01:59:50")
                 .addFormParam("T_DATETIME_PATTERN","2017-02-20 01:59:50")
                 .addFormParam("T_TIMESTAMP_PATTERN","2017-02-20 01:59:50.443")
-                .addFormParam("T_ISO8601_DATE_PATTERN","2017-02-20T01:59:50.96Z")
-                .addFormParam("T_RFC3339_DATE_PATTERN1","2017-02-20T01:59:50.960Z")
+                .addFormParam("T_ISO8601_DATE_PATTERN","2017-02-20T01:59:50.96+0800")
+                .addFormParam("T_RFC3339_DATE_PATTERN1","2017-02-20T01:59:50.960+0800")
                 .send().getContent();
         assertEquals("true",content);
     }
