3ac2d52a8c05aa2bfa53799f8a8c230cdda11b90
Kael <penghj@bingosoft.net>
2017-04-19 20:41:46 +0800

    fix jwt verify

--- base/core/src/main/java/leap/core/security/token/jwt/AbstractJwtVerifier.java
+++ base/core/src/main/java/leap/core/security/token/jwt/AbstractJwtVerifier.java
@@ -84,8 +84,8 @@ public abstract class AbstractJwtVerifier implements JwtVerifier {
     
     protected void verifyExpiration(Map<String, Object> claims) {
         Object exp = claims.get(JWT.CLAIM_EXPIRATION_TIME);
-        if (null != exp && exp instanceof Long) {
-            long expirationTimeSecond = (Long) exp;
+        if (null != exp && exp instanceof Number) {
+            long expirationTimeSecond = ((Number) exp).longValue();
             long nowTimeInSecond = System.currentTimeMillis()/1000L;
             if(expirationTimeSecond <= 0 || nowTimeInSecond >= expirationTimeSecond){
                 throw new TokenExpiredException("Token expired");
