5dfce4252b6c3dbc89604016a1d8bd14c4772578
calvin <244739873@qq.com>
2017-06-23 15:50:50 +0800

    [webapi]move test code to new project.

--- tests/leap-webapi-test/src/main/java/app/models/restd/Company.java
+++ tests/leap-webapi-test/src/main/java/app/models/restd/Company.java
@@ -1,31 +1,45 @@
 /*
- * Copyright 2016 the original author or authors.
+ * Copyright 2017 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *       http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
  */
 
-package app.models.petstore;
+package app.models.restd;
 
-public class Tag {
+import leap.orm.annotation.Id;
+import leap.orm.model.Model;
 
-    private Long   id   = null;
-    private String name = null;
+public class Company extends Model {
 
-    public Long getId() {
+    @Id
+    private String id;
+
+    private String name;
+
+    public Company() {
+    }
+
+    public Company(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getId() {
         return id;
     }
 
-    public void setId(Long id) {
+    public void setId(String id) {
         this.id = id;
     }
 
--- web/api/src/test/resources/doc/user_doc.md
+++
@@ -1,7 +0,0 @@
-# getAllWithoutPassword
-
-查询所有用户(没有密码)
-
-# enabled
-
-是否启用用户,没有启用的用户不能登录
