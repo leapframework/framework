17a93fd001d5abe31a2ac4261c994e1dd5d63d05
Kael <penghj@bingosoft.net>
2017-08-04 16:46:36 +0800

    fix oauth2 config parse server error

--- oauth2/webapp/src/main/java/leap/oauth2/webapp/DefaultOAuth2Config.java
+++ oauth2/webapp/src/main/java/leap/oauth2/webapp/DefaultOAuth2Config.java
@@ -181,7 +181,11 @@ public class DefaultOAuth2Config implements OAuth2Config, OAuth2Configurator, Ap
                     builder.append(getExpressionValue(exp,serverInfo));
                 }
             }else {
-                builder.append(c);
+                if(c != '/' || builder.length() == 0){
+                    builder.append(c);
+                }else if(builder.charAt(builder.length()-1)!='/'){
+                    builder.append(c);
+                }
             }
         }
         return builder.toString();
