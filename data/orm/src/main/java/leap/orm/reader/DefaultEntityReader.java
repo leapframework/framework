e08115c9dd2800df703119c14edabf6ce5df09c4
evanfeng <live.evan@gmail.com>
2018-07-12 16:04:58 +0800

    orm: fix null BeanProperty if the field mapping is not created by class mapping

--- data/orm/src/main/java/leap/orm/reader/DefaultEntityReader.java
+++ data/orm/src/main/java/leap/orm/reader/DefaultEntityReader.java
@@ -179,7 +179,7 @@ public class DefaultEntityReader implements EntityReader {
 			
 			if(null != fm && beanClass.equals(cm.getEntityMapping().getEntityClass())){
 				bp = fm.getBeanProperty();
-                if(!bp.getBeanType().getBeanClass().equals(beanClass)) {
+                if(null != bp && !bp.getBeanType().getBeanClass().equals(beanClass)) {
                     bp = null;
                 }
 			}else{
