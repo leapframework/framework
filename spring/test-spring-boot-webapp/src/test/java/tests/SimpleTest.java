0f919fa04ef7f6ca451deaaf6fc686e76a57b911
fenghm <live.evan@gmail.com>
2018-11-01 21:39:01 +0800

    add spring boot webapp test

---
+++ spring/test-spring-boot-webapp/src/test/java/tests/SimpleTest.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package tests;
+
+import leap.webunit.client.OkTHttpClient;
+import leap.webunit.client.THttpClient;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import tapp.Application;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+public class SimpleTest {
+
+    protected final THttpClient client = new OkTHttpClient(8080);
+
+    @Test
+    public void testIndex() {
+        client.get("/").assertContentContains("<h1>hello</h1>");
+        client.get("/index").assertContentContains("<h1>hello</h1>");
+    }
+
+    @Test
+    public void testStatic() {
+        client.get("/static/t.js").assertContentContains("function _(){}");
+    }
+}
