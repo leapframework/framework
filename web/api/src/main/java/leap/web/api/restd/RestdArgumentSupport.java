7551d92955d49086e0d571adec617cdcffc29098
liujinghui <liujinghui@bingosoft.net>
2020-06-28 10:56:36 +0800

    webapi: batch create argument default method

--- web/api/src/main/java/leap/web/api/restd/RestdArgumentSupport.java
+++ web/api/src/main/java/leap/web/api/restd/RestdArgumentSupport.java
@@ -24,6 +24,8 @@ public interface RestdArgumentSupport {
 
     default void processModelArgumentForCreate(RestdContext context, RestdModel model, ArgumentBuilder a){}
 
+    default void processModelArgumentForBatchCreate(RestdContext context, RestdModel model, ArgumentBuilder a){};
+
     default void processModelArgumentForUpdate(RestdContext context, RestdModel model, ArgumentBuilder a){}
 
     default void processModelArgumentForReplace(RestdContext context, RestdModel model, ArgumentBuilder a){}
