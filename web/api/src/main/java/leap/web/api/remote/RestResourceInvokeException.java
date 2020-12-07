8d4b04a0268d5354d55f543a86d04fde18fcc3a3
liujinghui <liujinghui@bingosoft.net>
2020-12-07 17:57:32 +0800

    webapi: try to get the message from the response of the rest resource invoke exception

--- web/api/src/main/java/leap/web/api/remote/RestResourceInvokeException.java
+++ web/api/src/main/java/leap/web/api/remote/RestResourceInvokeException.java
@@ -1,16 +1,20 @@
 package leap.web.api.remote;
 
+import leap.lang.Strings;
+import leap.lang.http.ContentTypes;
 import leap.lang.http.client.HttpResponse;
+import leap.lang.json.JSON;
 import leap.web.Content;
 import leap.web.Request;
 import leap.web.Response;
+import leap.web.api.mvc.ApiError;
 import leap.web.exception.ResponseException;
 
 public class RestResourceInvokeException extends ResponseException {
     private final HttpResponse response;
 
     public  RestResourceInvokeException(HttpResponse response){
-        super(response.getStatus(), String.valueOf(response.getStatus()), new Content() {
+        super(response.getStatus(), tryGetErrorMessage(response), new Content() {
             @Override
             public String getContentType(Request request) throws Throwable {
                 return response.getContentType().toString();
@@ -26,5 +30,17 @@ public class RestResourceInvokeException extends ResponseException {
 
     public HttpResponse getResponse() {
         return response;
+    }
+
+    private static String tryGetErrorMessage(HttpResponse response) {
+        String content = response.getString();
+        if (!Strings.isEmpty(content) && ContentTypes.APPLICATION_JSON_TYPE.isCompatible(response.getContentType())) {
+            try {
+                return JSON.decode(content, ApiError.class).getMessage();
+            } catch (Exception ignored) {
+
+            }
+        }
+        return String.valueOf(response.getStatus());
     }
 }
